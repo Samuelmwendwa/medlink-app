a6d015673022202ab10d698ea92fe380326376f1
Samuelmwendwa <162443747+Samuelmwendwa@users.noreply.github.com>
2025-03-17 19:38:24 +0300

    Create ActiveAlertCard.tsx.tsx

---
+++ app/tempobook/dynamic/app/auth/components/tempobook/storyboards/components/dashboard/ActiveAlertCard.tsx.tsx
@@ -0,0 +1,10 @@
+
+            import ActiveAlertCard from "./../../../../../components/dashboard/ActiveAlertCard.tsx";
+
+            const TempoComponent = () => {
+              return <ActiveAlertCard />;
+            }
+
+            
+
+            export default TempoComponent;
