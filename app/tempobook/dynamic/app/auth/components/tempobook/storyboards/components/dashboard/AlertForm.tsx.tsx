983bad15e3d6ccaf1283382cda27da69fa527675
Samuelmwendwa <162443747+Samuelmwendwa@users.noreply.github.com>
2025-03-17 19:39:44 +0300

    Create AlertForm.tsx.tsx

---
+++ app/tempobook/dynamic/app/auth/components/tempobook/storyboards/components/dashboard/AlertForm.tsx.tsx
@@ -0,0 +1,10 @@
+
+            import AlertForm from "./../../../../../components/dashboard/AlertForm.tsx";
+
+            const TempoComponent = () => {
+              return <AlertForm />;
+            }
+
+            
+
+            export default TempoComponent;
