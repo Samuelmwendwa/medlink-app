309470dcb09b665a7ffb32ff1df86b1149bfe0ae
Samuelmwendwa <162443747+Samuelmwendwa@users.noreply.github.com>
2025-03-17 19:42:24 +0300

    Create EmergencyAlerts.tsx.tsx

---
+++ app/tempobook/dynamic/app/auth/components/tempobook/storyboards/components/dashboard/EmergencyAlerts.tsx.tsx
@@ -0,0 +1,10 @@
+
+            import EmergencyAlerts from "./../../../../../components/dashboard/EmergencyAlerts.tsx";
+
+            const TempoComponent = () => {
+              return <EmergencyAlerts />;
+            }
+
+            
+
+            export default TempoComponent;
