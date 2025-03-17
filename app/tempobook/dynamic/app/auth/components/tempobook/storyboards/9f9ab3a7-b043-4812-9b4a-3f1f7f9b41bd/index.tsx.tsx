e394e7f7e30f20dd9c6678aed5f13c7a22263b2b
Samuelmwendwa <162443747+Samuelmwendwa@users.noreply.github.com>
2025-03-17 19:31:47 +0300

    Create index.tsx.tsx

---
+++ app/tempobook/dynamic/app/auth/components/tempobook/storyboards/9f9ab3a7-b043-4812-9b4a-3f1f7f9b41bd/index.tsx.tsx
@@ -0,0 +1,10 @@
+
+            import LoginPageStoryboard from "./../../../../../storyboards/9f9ab3a7-b043-4812-9b4a-3f1f7f9b41bd/index.tsx";
+
+            const TempoComponent = () => {
+              return <LoginPageStoryboard />;
+            }
+
+            
+
+            export default TempoComponent;
