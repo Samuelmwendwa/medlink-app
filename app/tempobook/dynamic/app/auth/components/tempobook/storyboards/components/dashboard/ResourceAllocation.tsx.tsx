665a67efa96d226f168571a2f3d848c556fc6b11
Samuelmwendwa <162443747+Samuelmwendwa@users.noreply.github.com>
2025-03-17 19:44:20 +0300

    Create  ResourceAllocation.tsx.tsx

---
+++ app/tempobook/dynamic/app/auth/components/tempobook/storyboards/components/dashboard/ResourceAllocation.tsx.tsx
@@ -0,0 +1,10 @@
+
+            import ResourceAllocation from "./../../../../../components/dashboard/ResourceAllocation.tsx";
+
+            const TempoComponent = () => {
+              return <ResourceAllocation />;
+            }
+
+            
+
+            export default TempoComponent;
