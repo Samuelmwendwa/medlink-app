918d0fa128ecbfdd7e865c6e1c0cd3af0367544e
Samuelmwendwa <162443747+Samuelmwendwa@users.noreply.github.com>
2025-03-17 19:41:07 +0300

    Create BedManagement.tsx.tsx

---
+++ app/tempobook/dynamic/app/auth/components/tempobook/storyboards/components/dashboard/BedManagement.tsx.tsx
@@ -0,0 +1,10 @@
+
+            import BedManagement from "./../../../../../components/dashboard/BedManagement.tsx";
+
+            const TempoComponent = () => {
+              return <BedManagement />;
+            }
+
+            
+
+            export default TempoComponent;
