2b05c2007ebeb95bf390d3cd588c088207276c14
Samuelmwendwa <162443747+Samuelmwendwa@users.noreply.github.com>
2025-03-17 19:41:44 +0300

    Create BedStatusCard.tsx.tsx

---
+++ app/tempobook/dynamic/app/auth/components/tempobook/storyboards/components/dashboard/BedStatusCard.tsx.tsx
@@ -0,0 +1,10 @@
+
+            import BedStatusCard from "./../../../../../components/dashboard/BedStatusCard.tsx";
+
+            const TempoComponent = () => {
+              return <BedStatusCard />;
+            }
+
+            
+
+            export default TempoComponent;
