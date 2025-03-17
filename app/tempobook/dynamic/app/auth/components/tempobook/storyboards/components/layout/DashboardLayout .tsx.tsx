f6318d5b8142cad838fd037b1c76607c35de636e
Samuelmwendwa <162443747+Samuelmwendwa@users.noreply.github.com>
2025-03-17 19:46:25 +0300

    Create DashboardLayout .tsx.tsx

---
+++ app/tempobook/dynamic/app/auth/components/tempobook/storyboards/components/layout/DashboardLayout .tsx.tsx
@@ -0,0 +1,10 @@
+
+            import DashboardLayout from "./../../../../../components/layout/DashboardLayout.tsx";
+
+            const TempoComponent = () => {
+              return <DashboardLayout />;
+            }
+
+            
+
+            export default TempoComponent;
