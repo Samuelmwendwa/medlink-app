c8357e2ecebc96a27d0991937637e05663a0738b
Samuelmwendwa <162443747+Samuelmwendwa@users.noreply.github.com>
2025-03-17 19:43:00 +0300

    Create PatientCard.tsx.tsx

---
+++ app/tempobook/dynamic/app/auth/components/tempobook/storyboards/components/dashboard/PatientCard.tsx.tsx
@@ -0,0 +1,10 @@
+
+            import PatientCard from "./../../../../../components/dashboard/PatientCard.tsx";
+
+            const TempoComponent = () => {
+              return <PatientCard />;
+            }
+
+            
+
+            export default TempoComponent;
