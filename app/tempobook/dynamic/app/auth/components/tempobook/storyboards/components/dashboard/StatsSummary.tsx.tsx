2a4245ad2224661fadb02e53d8e9adbbd65b86ef
Samuelmwendwa <162443747+Samuelmwendwa@users.noreply.github.com>
2025-03-17 19:45:30 +0300

    Create StatsSummary.tsx.tsx

---
+++ app/tempobook/dynamic/app/auth/components/tempobook/storyboards/components/dashboard/StatsSummary.tsx.tsx
@@ -0,0 +1,10 @@
+
+            import StatsSummary from "./../../../../../components/dashboard/StatsSummary.tsx";
+
+            const TempoComponent = () => {
+              return <StatsSummary />;
+            }
+
+            
+
+            export default TempoComponent;
