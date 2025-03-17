412fe7a06a2b6307a5c11a77721392855e216461
Samuelmwendwa <162443747+Samuelmwendwa@users.noreply.github.com>
2025-03-17 19:46:59 +0300

    Create Header.tsx.tsx

---
+++ app/tempobook/dynamic/app/auth/components/tempobook/storyboards/components/layout/Header.tsx.tsx
@@ -0,0 +1,10 @@
+
+            import Header from "./../../../../../components/layout/Header.tsx";
+
+            const TempoComponent = () => {
+              return <Header />;
+            }
+
+            
+
+            export default TempoComponent;
